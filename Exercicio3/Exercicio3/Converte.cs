56107b3858981034a8124ffb5403e820b33a5429
NecroShade1829 <56036903+NecroShade1829@users.noreply.github.com>
2020-03-30 17:57:48 +0100

    Exercicio 3 "Temperature"

---
+++ Exercicio3/Exercicio3/Converte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio3
+{
+   public static class Converte
+    {
+        
+        public static string CelsiusToFah(int grauCel)
+        {
+            double grauEmFah = grauCel * 9/5 + 32;
+            return String.Format("{0}",grauEmFah);
+        }
+
+        public static string FahrenheitToCel(int grauFah)
+        {
+            double grauEmCel = (grauFah - 32) * (5 / 9);
+            return String.Format("{0}",grauEmCel);
+        }
+    }
+}
---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] comandoPalavra = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (comandoPalavra[1])
+                {
+                    case "Celsius": Console.WriteLine(Converte.CelsiusToFah(int.Parse(comandoPalavra[0])));  break;
+                    case "Fahrenheit": Console.WriteLine(Converte.FahrenheitToCel(int.Parse(comandoPalavra[0]))); break;
+                    default: Console.WriteLine("unidade nao valida"); break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
