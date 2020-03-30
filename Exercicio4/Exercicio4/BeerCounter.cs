f653d65924677c3cac6a215946c897a8aee1c79a
NecroShade1829 <56036903+NecroShade1829@users.noreply.github.com>
2020-03-30 17:59:17 +0100

    Exercicio 4 "Beer"

---
+++ Exercicio4/Exercicio4/BeerCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio4
+{
+    public static class BeerCounter
+    {
+        public static int beerInStock;
+        public static int beerDrankCount;
+
+        public static void Beer(int bottleCount)
+        {
+            beerInStock += bottleCount;
+        }
+
+        public static void DrankBeer(int bottleCount)
+        {
+            beerDrankCount += bottleCount;
+            beerInStock -= bottleCount;
+        }
+    }
+}
---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while(command != "End")
+            {
+                string[] comandoPalavra = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                int BuyBeer = int.Parse(comandoPalavra[0]);
+                int DrinkBeer = int.Parse(comandoPalavra[1]);
+                BeerCounter.Beer(BuyBeer);
+                BeerCounter.DrankBeer(DrinkBeer);
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(BeerCounter.beerInStock + " " + BeerCounter.beerDrankCount);
+        }
+    }
+}
