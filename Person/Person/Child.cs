b60d2d0026c5acdaa26a0d2cc12875bed85fa4d6
NecroShade1829 <56036903+NecroShade1829@users.noreply.github.com>
2020-04-20 14:27:57 +0100

    Ex Person

---
+++ Person/Person/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child:Pessoa
+    {
+        protected override int Age 
+        { 
+            get => base.Age;
+
+            set
+            {
+                if(value>15) throw new ArgumentException("Child's age must be less than 15!");
+                base.Age = value;
+            }
+        }
+        public Child(string name, int age) : base(name, age)
+        {           
+            base.Name = name;
+            base.Age = Age;
+        }
+    }
+}
---
+++ Person/Person/Pessoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+   public class Pessoa
+    {
+        private string name;
+        private int age;
+
+        protected virtual string Name
+        {
+            get => name;
+            set
+            {
+                if (value.Length <=2) throw new ArgumentException("Please insert a valid name!");
+                name = value;
+            }
+        }
+
+        protected virtual int Age
+        {
+            get => age;
+            set
+            {
+                if (value <= 0) throw new ArgumentException("Please insert a real age!");
+
+                age = value;
+            }
+        }
+        
+        protected Pessoa (string nome, int idade)
+        {
+            Name = nome;
+            Age = idade;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format($"Name: {name}, Age: {age}"));
+            return stringBuilder.ToString();
+        }
+    }
+}
